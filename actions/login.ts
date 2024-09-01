d4fbc1ac8a74b1644b6ba6fda4c4c4afd07f816a
YevhenPetrovskyi <71007428+YevhenPetrovskyi@users.noreply.github.com>
2024-09-01 21:40:18 +0300

    base styles for register page

--- actions/login.ts
+++ actions/login.ts
@@ -3,7 +3,6 @@
 import * as z from 'zod';
 
 import { LoginSchema } from '@/schemas';
-import { error } from 'console';
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
   const validatedFields = LoginSchema.safeParse(values);
