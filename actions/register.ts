d4a937433d1148ac19836833dbb8ccdf41f09cbb
YevhenPetrovskyi <71007428+YevhenPetrovskyi@users.noreply.github.com>
2024-09-06 10:37:34 +0300

    add User schema and registration logic

--- actions/register.ts
+++ actions/register.ts
@@ -1,6 +1,10 @@
 'use server';
 
+import bcrypt from 'bcrypt';
 import * as z from 'zod';
+
+import { db } from '@/lib/db';
+import { getUserByEmail } from '@/data/user';
 
 import { RegisterSchema } from '@/schemas';
 
@@ -11,5 +15,24 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     return { error: 'Invalid fields!' };
   }
 
-  return { success: 'Email was sent!' };
+  const { email, password, name } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: 'Email already in use!' };
+  }
+
+  await db.user.create({
+    data: {
+      email,
+      name,
+      password: hashedPassword,
+    },
+  });
+
+  // TODO: Send verification token email
+
+  return { success: 'User created!' };
 };
