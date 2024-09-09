f8c41e2ef4152ff21688854071184383434d5b61
YevhenPetrovskyi <71007428+YevhenPetrovskyi@users.noreply.github.com>
2024-09-09 12:19:31 +0300

    add signIn

--- actions/login.ts
+++ actions/login.ts
@@ -2,7 +2,9 @@
 
 import * as z from 'zod';
 
+import { signIn } from '@/auth';
 import { LoginSchema } from '@/schemas';
+import { DEFAULT_LOGIN_REDIRECT } from '@/routes';
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
   const validatedFields = LoginSchema.safeParse(values);
@@ -11,5 +13,15 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
     return { error: 'Invalid fields!' };
   }
 
-  return { success: 'Email was sent' };
+  const { email, password } = validatedFields.data;
+
+  try {
+    await signIn('credentials', {
+      email,
+      password,
+      redirectTo: DEFAULT_LOGIN_REDIRECT,
+    });
+  } catch (error) {
+    // TODO: Handle error
+  }
 };
